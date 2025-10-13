52971fdf6d9cef467368a22eeb27ad3542bd1036
Lahiru <lahirum@pickme.lk>
2025-10-13 19:45:51 +0530

    font issue

--- src/api/certificates/handler.ts
+++ src/api/certificates/handler.ts
@@ -83,16 +83,17 @@ export async function certificateHandler(request: Request, h: ResponseToolkit) {
       svgUrl:
         '/api/countryconfig/certificates/birth-certificate-certified-copy.svg',
       fonts: {
-        'Noto Sans': {
-          // normal: '/api/countryconfig/fonts/NotoSans-Regular.ttf',
-          // bold: '/api/countryconfig/fonts/NotoSans-Bold.ttf',
-          // italics: '/api/countryconfig/fonts/NotoSans-Regular.ttf',
-          // bolditalics: '/api/countryconfig/fonts/NotoSans-Regular.ttf'
-
-          normal: '/api/countryconfig/fonts/TimesNewRoman-Regular.ttf',
-          bold: '/api/countryconfig/fonts/TimesNewRoman-Bold.ttf',
-          italics: '/api/countryconfig/fonts/TimesNewRoman-Regular.ttf',
-          bolditalics: '/api/countryconfig/fonts/TimesNewRoman-Regular.ttf'
+        // 'Noto Sans': {
+        //   normal: '/api/countryconfig/fonts/NotoSans-Regular.ttf',
+        //   bold: '/api/countryconfig/fonts/NotoSans-Bold.ttf',
+        //   italics: '/api/countryconfig/fonts/NotoSans-Regular.ttf',
+        //   bolditalics: '/api/countryconfig/fonts/NotoSans-Regular.ttf'
+        // }
+        'Libre Baskerville': {
+          normal: '/api/countryconfig/fonts/LibreBaskerville-Regular.ttf',
+          bold: '/api/countryconfig/fonts/LibreBaskerville-Bold.ttf',
+          italics: '/api/countryconfig/fonts/LibreBaskerville-Italic.ttf',
+          bolditalics: '/api/countryconfig/fonts/LibreBaskerville-Regular.ttf'
         }
       }
     },
